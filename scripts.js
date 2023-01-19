3c6aacf632da4424a49104b1bf0077cb1dc03d44
aldodamacena <aldodamacena2@gmail.com>
2023-01-18 21:45:27 -0300

    movidopara pasta assets

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
-const button = document.getElementById('convert-button')
-const select = document.getElementById('currency-select')
+const button = document.getElementById("convert-button")
+const select = document.getElementById("currency-select")
 
 const dolar = 5.2
 
 const convertValues = () => {
-      const inputReais = document.getElementById('input-real').value
-      const realValueText = document.getElementById('real-value-text')
-      const currencyValueText = document.getElementById('currency-value-text')
+      const inputReais = document.getElementById("input-real").value
+      const realValueText = document.getElementById("real-value-text")
+      const currencyValueText = document.getElementById("currency-value-text")
 
       
       
@@ -23,12 +23,18 @@ currencyValueText.innerHTML = new Intl.NumberFormat("en-US",{
 }
 changeCurrency =() => {
   const currencyName = document.getElementById("currency-name")
-
+  const currencyImg = document.getElementById("currency-img")
+ 
+  if(select.value === 'US$ Dólar americano') {
+    currencyName.innerHTML = "Dólar americano"
+    currencyImg.src ="./assets/estados-unidos (1) 1.svg"
+  }
+  
   if(select.value === '€ Euro') {
     currencyName.innerHTML = "Euro"
-    currencyImg.src="./Design sem nome 1.svg"
+    currencyImg.src ="./assets/Design sem nome 1.svg"
   }
 }
 
-button.addEventListener('click', convertValues)
-select.addEventListener('change',changeCurrency)
+button.addEventListener("click", convertValues)
+select.addEventListener("change",changeCurrency)
