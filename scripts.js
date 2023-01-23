1ce0f4a6a1ff6923d49af209cace64f5ac842298
aldodamacena <aldodamacena2@gmail.com>
2023-01-23 20:04:56 -0300

    agora

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const button = document.getElementById("convert-button")
 const select = document.getElementById("currency-select")
 
 const dolar = 5.2
+const euro = 5.9
 
 const convertValues = () => {
       const inputReais = document.getElementById("input-real").value
@@ -15,10 +16,20 @@ realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
   currency:"BRL",
 }).format(inputReais)
 
-currencyValueText.innerHTML = new Intl.NumberFormat("en-US",{
-  style: "currency",
-  currency: "USD",
-}).format(inputReais / dolar)
+if(select.value === "US$ Dólar americano" ) {
+  currencyValueText.innerHTML = new Intl.NumberFormat("en-US",{
+    style: "currency",
+    currency: "USD",
+  }).format(inputReais / dolar)
+}
+if(select.value === "€ Euro"){
+  currencyValueText.innerHTML = new Intl.NumberFormat("de-DE",{
+    style: "currency",
+    currency: "EUR",
+  }).format(inputReais / euro)
+}
+
+
       
 }
 changeCurrency =() => {
